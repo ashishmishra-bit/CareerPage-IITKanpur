7a9367094e45c18a8f1bebee80989764734d2590
nks102000 <nitishks1110@gmail.com>
2021-05-12 17:58:08 +0530

    home page made

--- public/js/app.js
+++ public/js/app.js
@@ -8,13 +8,18 @@
 /***/ (() => {
 
 window.addEventListener("scroll", function () {
-  var menuArea = document.getElementById('menu-area');
+  var menuArea = document.getElementById("menu-area");
 
   if (window.pageYOffset > 0) {
     menuArea.classList.add("sc-menu");
   } else {
     menuArea.classList.remove("sc-menu");
   }
+});
+$(document).ready(function () {
+  $('#icon').click(function () {
+    $('ul').toggleClass('show');
+  });
 });
 
 /***/ }),
